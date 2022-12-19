0e1650fe39e4ce2efe3631f890f3df7cf82027fc
Gabriel Ventorim Silva <106477470+GabsVentorim@users.noreply.github.com>
2022-12-19 17:16:18 -0300

    Update QuickSort.c

--- QuickSort/QuickSort.c
+++ QuickSort/QuickSort.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void troca(int *a, int* b){
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
 int particao(int A[], int ini, int ref){
     int x = A[ref];
     int i = ini - 1;
@@ -15,7 +21,7 @@ int particao(int A[], int ini, int ref){
     return (i + 1);
 }
 
-void quitSort(int *A[], int ini, int ref){
+void quitSort(int A[], int ini, int ref){
     if(ini < ref){
         int q = particao(&A, ini, ref);
         quitSort(&A, ini, q - 1);
@@ -24,4 +30,6 @@ void quitSort(int *A[], int ini, int ref){
 }
 
 void main(){
+    int A[5] = {2, 10, 1, 4, 7}
+    quickSort(&A, 0, A[4]);
 }
