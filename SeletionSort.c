4ff6ae4daba62c2875e43b99307e81e7e8f86f60
Gabriel Ventorim Silva <106477470+GabsVentorim@users.noreply.github.com>
2022-12-19 17:23:44 -0300

    Update SeletionSort.c

--- SeletionSort.c
+++ SeletionSort.c
@@ -1,1 +1,20 @@
-    
+#include <stdio.h>
+
+void troca(int *a, int *b){
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+// n -> number of elements
+void SelectionSort(int A[], int n){
+    int i, j, min, x;
+    for (i = 0; i < n ; i++){
+        min = i;
+        for (j = i + 1; j < n; j++){
+            if (A[j]) < (A[min]))
+                min = j;
+        }
+        troca(&(A[min]), &(A[i]));
+    }
+}
